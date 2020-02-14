f68e44e54a0c1457a0f0aea79fbfdc4ec134fed9
Lucas Overland <lucas.overland@gmail.com>
2020-02-13 21:36:58 -0600

    some more stuff to save

--- save.rb
+++ save.rb
@@ -2,9 +2,12 @@
 # takes screenshot using Firefox
 # commits screenshot to new git repo
 require 'date'
+require 'json'
 
 d = DateTime.now
-current_date = d.year.to_s.concat("-", d.month.to_s, "-", d.day.to_s)
+screenshot_name = d.strftime("%Y-%m-%d")
+current_date = d.strftime("%d %B")
+current_year = d.strftime("%Y")
 
 puts "Loading jekyll site..."
 
@@ -14,9 +17,21 @@ sleep 3
 
 puts "jekyll site loaded."
 
-fork { exec("/mnt/c/Program\\ Files/Firefox\\ Nightly/firefox.exe -headless --screenshot '..\\website.overlandandseas\\screenshots\\".concat(current_date, ".jpg' 'https://overlandandseas.com'")) }
+fork { exec("/mnt/c/Program\\ Files/Firefox\\ Nightly/firefox.exe -headless --screenshot '..\\website.overlandandseas\\screenshots\\".concat(screenshot_name, ".jpg' 'https://overlandandseas.com'")) }
 
 puts "Screenshot printed"
 
 Process.kill(9, pid)
 puts "server killed"
+
+# load data from file
+screenshots_file = File.read "screenshots.json"
+screenshots_hash = JSON.parse file
+
+screenshots_hash.push({
+  "name": screenshot_name,
+  "date": current_date,
+  "year": current_year,
+  "homepagefilename": screenshot_name.concat(".jpg"),
+  "postfileaname": screenshot_name.concat(".jpg")
+})
